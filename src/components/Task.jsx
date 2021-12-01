a2a350e18c5eb3b0dbc6728a4365dc54f0789c47
leandromoura <leandro.moura@sellbie.com.br>
2021-12-01 08:15:58 -0300

    primeiro commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { CgClose, CgInfo } from "react-icons/cg";
+import "./Task.css";
+import { useHistory } from "react-router-dom";
+
+const Task = ({ task, handleTaskClick, handleTaskDelete }) => {
+  const history = useHistory();
+
+  const handleTaskDetailsClick = () => {
+    history.push(`/${task.title}`);
+  };
+  return (
+    <div
+      className="task-container"
+      style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
+    >
+      <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+        {task.title}
+      </div>
+
+      <div className="buttons-container">
+        <button
+          className="remove-task-button"
+          onClick={() => handleTaskDelete(task.id)}
+        >
+          <CgClose />
+        </button>
+        <button
+          className="see-task-details-button"
+          onClick={handleTaskDetailsClick}
+        >
+          <CgInfo />
+        </button>
+      </div>
+    </div>
+  );
+  // return <div className="task-container">{task.title}</div>;
+};
+
+export default Task;
